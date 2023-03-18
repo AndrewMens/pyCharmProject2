8b1284cf0b54d91772705d89edba1c0329fdfffb
AndrewMens <ungrey85@mail.ru>
2023-03-18 16:01:27 +0300

    commit pycharm qap task 14.5.15

--- qap 12.7.3.py
+++ qap 12.7.3.py
@@ -1,5 +1,12 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money = int(input('Введите сумму, которую планируете положить под проценты: '))
-deposit = list(map(lambda x:int(x*money/100), per_cent.values()))
-print('deposit =', deposit)
-print('Максимальная сумма, которую вы можете заработать — ', max(deposit))
+deposit = list(map(lambda x:float(x*money/100), per_cent.values()))
+def f(deposit):
+    for i in deposit:
+        if i-int(i)==0:return int(i)
+
+        else:return i
+# deposit1 = list(map())
+# [return int(i) if i-int(i)==0 else:return i for i in ]
+print('deposit =', f(deposit))
+# print('Максимальная сумма, которую вы можете заработать — ', max(f(deposit)))
--- qap 13.8.19.py
+++ qap 13.8.19.py
@@ -1,11 +1,18 @@
 check = 0
+cost0 = 0
+cost18 = 990
+cost25 = 1390
 ticket = int(input('Введите количество билетов, которое хотите приобрести: '))
-age = [int(input('Введите количество лет посетителя конференции: ')) for i in range(ticket)]
+age = [int(input('Введите возраст посетителя конференции: ')) for i in range(ticket)]
 for a in age:
+    if 0 <= a < 18:
+        check += cost0
     if 18 <= a < 25:
-        check += 990
-    if a >= 25:
-        check += 1390
+        check += cost18
+    elif 25 <= a < 125:
+        check += cost25
+    else:
+        print('возраст указан неверно')
 if ticket > 3:
     check -= check / 10
 print(f'Сумма к оплате {int(check)} руб.')
