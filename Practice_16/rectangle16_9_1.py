09ab6c835b5c1a6867adab939eb353e96a299022
AndrewMens <ungrey85@mail.ru>
2023-04-02 17:50:56 +0300

    commit pycharm qap task 16.9.4

--- Practice_16/rectangle16_9_1.py
+++ Practice_16/rectangle16_9_1.py
@@ -8,6 +8,10 @@ class Rectangle:
     def __str__(self):
         return f"Rectangle: {self.x}, {self.y}, {self.widht}, {self.height}"
 
+    def getArea(self):
+        return self.widht*self.height
+
 rec = Rectangle(5, 10, 50, 100)
 
 print(rec)
+print(rec.getArea())
