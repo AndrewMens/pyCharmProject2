0610dd43f5909541a61037c9c1618fc3354cbf8a
AndrewMens <ungrey85@mail.ru>
2023-04-02 17:07:47 +0300

    commit pycharm qap task 16.9.1

---
+++ Practice_16/rectangle16_9_1.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    def __init__(self,x,y,widht,height):
+        self.x = x
+        self.y = y
+        self.widht = widht
+        self.height = height
+
+    def __str__(self):
+        return f"Rectangle: {self.x}, {self.y}, {self.widht}, {self.height}"
+
+rec = Rectangle(5, 10, 50, 100)
+
+print(rec)
